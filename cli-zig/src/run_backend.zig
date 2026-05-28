40f7faad7973b40ec5b0ee227a91be60893d3c1c
ecetinerdem <cetoerdem1@gmail.com>
2026-05-28 12:01:30 +0400

    launch go server

--- cli-zig/src/run_backend.zig
+++ cli-zig/src/run_backend.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const flags = @import("flags.zig");
 
 pub fn runBackendServer(io: std.Io, allocator: std.mem.Allocator, args: flags.Args) !u8 {
-    const go_path = "../backend/main.go";
+    const go_path = "main.go";
 
     var argv: std.ArrayList([]const u8) = .empty;
     defer argv.deinit(allocator);
@@ -27,25 +27,24 @@ pub fn runBackendServer(io: std.Io, allocator: std.mem.Allocator, args: flags.Ar
 
     var child = try std.process.spawn(io, .{
         .argv = argv.items,
+        .stdout = .inherit,
+        .stderr = .inherit,
+        .cwd = .{ .path = "../backend" },
     });
 
     std.debug.print("Running Go backend: {s}...\n", .{go_path});
 
-    const exit_code = try child.wait(io);
+    // var buf: [256]u8 = undefined;
+    // var file_reader = child.stdout.?.reader(io, &buf);
 
-    return switch (exit_code) {
-        .exited => |code| code,
-        .signal => |sig| {
-            std.debug.print("Go server was killed by signal {}\n", .{sig});
-            return 1;
-        },
-        .stopped => |sig| {
-            std.debug.print("Go server was stopped by signal {}\n", .{sig});
-            return 1;
-        },
-        .unknown => |code| {
-            std.debug.print("Go server exited with unknown status {}\n", .{code});
-            return 1;
-        },
-    };
+    // while (try file_reader.interface.takeDelimiter('\n')) |line| {
+    //     std.debug.print("[backend] {s}\n", .{line});
+
+    //     if (std.mem.indexOf(u8, line, "Server running on:") != null) {
+    //         std.debug.print("Backend is ready!\n", .{});
+    //         break;
+    //     }
+    // }
+    _ = try child.wait(io);
+    return 0;
 }
